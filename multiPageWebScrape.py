38d4e7b83e5a9e7b67e4a39af323de2fafd65a39
Tahmidol Alom <32310776+T4HM1D@users.noreply.github.com>
2022-05-30 16:58:37 +0100

    multipage supported

--- multiPageWebScrape.py
+++ multiPageWebScrape.py
@@ -1,38 +1,71 @@
 import requests
 from bs4 import BeautifulSoup
-# url = "https://www.keychron.uk/collections/keychron-uk-iso-layout-keyboards"
-url = "https://www.keychron.uk/collections/new-arrivals"
+url = "https://www.keychron.uk/collections/all-products"
+
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'lxml')
 priceFile = open('priceFile.txt', 'w')
 
-itemsContent = soup.find_all('div', class_='grid-product__content')
-itemPrice = soup.find_all('div', class_='grid-product__price')
-ogPrice = itemPrice[1].find('span', class_='grid-product__price--original')
-salePrice = itemPrice[1].find('span', class_='sale-price').text.strip()
-# print(itemPrice[0].text.strip())
-# print(salePrice)
-# i = itemPrice[1]
-# if i.find('span', class_='sale-price') != None:
-#     print(i.find('span', class_='sale-price').text.strip())
-# else:
-#     print(i.text.strip())
+# itemsContent = soup.find_all('div', class_='grid-product__content')
+# itemPrice = soup.find_all('div', class_='grid-product__price')
+# for i in range(len(itemsContent)):
+#     itemName = itemsContent[i].find('div', class_='grid-product__title').text
+#     print(itemName)
+#     price = itemPrice[i]
+#     if price.find('span', class_='sale-price') != None:
+#         sale = price.find('span', class_='sale-price').text.strip()
+#         og = price.find('span', class_='grid-product__price--original').text
+#         saleString = "\nSale price: %s, original price: %s\n" % (sale, og)
+#         print(saleString)
+#         priceFile.write(itemName)
+#         priceFile.write(saleString)
 
-for i in range(len(itemsContent)):
-    itemName = itemsContent[i].find('div', class_='grid-product__title').text
-    print(itemName)
-    price = itemPrice[i]
-    if price.find('span', class_='sale-price') != None:
-        sale = price.find('span', class_='sale-price').text.strip()
-        og = price.find('span', class_='grid-product__price--original').text
-        saleString = "\n Sale price: %s, original price: %s \n" % (sale, og)
-        print(saleString)
-        priceFile.write(itemName)
-        priceFile.write(saleString)
+#     else:
+#         normalPrice = price.text.strip()
+#         normalString = "\nRegular Price: %s\n" % (normalPrice)
+#         print(normalString)
+#         priceFile.write(itemName)
+#         priceFile.write(normalString)
 
-    else:
-        normalPrice = price.text.strip()
-        normalString = "\n Regular Price: %s \n" % (normalPrice)
-        print(normalString)
-        priceFile.write(itemName)
-        priceFile.write(normalString)
+
+pages = soup.find('div', class_='pagination')
+urls = []
+links = pages.find_all('span', class_='page')
+for i in range(len(links)):
+    tags = links[i].find_all('a')
+    for tag in tags:
+        pageNum = int(tag.text) if tag.text.isdigit() else None
+        if pageNum != None:
+            split = tag.get('href').split('products')
+            print(split)
+            x = split[-1]
+            urls.append(x)
+print(urls)
+
+for i in urls:
+    newUrl = url + i
+    response = requests.get(newUrl)
+    soup = BeautifulSoup(response.text, 'lxml')
+
+    itemsContent = soup.find_all('div', class_='grid-product__content')
+    itemPrice = soup.find_all('div', class_='grid-product__price')
+
+    for j in range(len(itemsContent)):
+        itemName = itemsContent[j].find(
+            'div', class_='grid-product__title').text
+        print(itemName)
+        price = itemPrice[i]
+        if price.find('span', class_='sale-price') != None:
+            sale = price.find('span', class_='sale-price').text.strip()
+            og = price.find(
+                'span', class_='grid-product__price--original').text
+            saleString = "\nSale price: %s, original price: %s\n" % (sale, og)
+            print(saleString)
+            priceFile.write(itemName)
+            priceFile.write(saleString)
+        else:
+            normalPrice = price.text.strip()
+            normalString = "\nRegular Price: %s\n" % (normalPrice)
+            print(normalString)
+            priceFile.write(itemName)
+            priceFile.write(normalString)
